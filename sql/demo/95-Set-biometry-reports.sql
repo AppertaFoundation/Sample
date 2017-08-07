39e459b8cea7e649c8a7d899476bb6eeb0a1d319
biskyt <toby.bisco@openeyes.org.uk>
2017-08-07 12:15:31 +0100

    linked biom report for charles darwin

---
+++ sql/demo/95-Set-biometry-reports.sql
@@ -0,0 +1,4 @@
+use openeyes;
+
+UPDATE `openeyes`.`event` SET `episode_id`='600874', `event_date`='2017-08-04 10:15:44' WHERE `id`='3686313';
+UPDATE `openeyes`.`ophinbiometry_imported_events` SET `is_linked`='1' WHERE `id`='20';
