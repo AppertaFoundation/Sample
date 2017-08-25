64b709a16779abc5f218411f87994613f6362c6c
biskyt <toby.bisco@openeyes.org.uk>
2017-08-25 10:56:35 +0100

    use php in front of yiic comands

--- sql/demo/95-visualfieldsimport.sh
+++ sql/demo/95-visualfieldsimport.sh
@@ -12,7 +12,7 @@ mkdir runtime/fields/dups
 
 cp modules/OphInVisualfields/tests/fields/legacy/* runtime/fields/legacy/
 
-./yiic importlegacyvf import --importDir=runtime/fields/legacy --archiveDir=runtime/fields/out --errorDir=runtime/fields/err --dupDir=runtime/fields/dups
+php ./yiic importlegacyvf import --importDir=runtime/fields/legacy --archiveDir=runtime/fields/out --errorDir=runtime/fields/err --dupDir=runtime/fields/dups
 
 chown -R www-data:www-data files
 chmod -R 774 files
--- sql/demo/yiiccommands.sh
+++ sql/demo/yiiccommands.sh
@@ -4,9 +4,9 @@ dir=$PWD
 cd /var/www/openeyes/protected
 
 echo "  - Generating Unique Codes..."
-./yiic generateuniquecode
+php ./yiic generateuniquecode
 
 echo "  - Generating Operation Booking Sessions"
-./yiic generatesessions
+php ./yiic generatesessions
 
 cd "$dir"
