8b98646334c2f28f6cf81f6e2d890d23dd5205bb
biskyt <toby.bisco@openeyes.org.uk>
2017-09-01 16:50:22 +0100

    some minor fixes to make testing operation notes easier for Charles Darwin

--- sql/demo/Demofixes.sql
+++ sql/demo/Demofixes.sql
@@ -3,3 +3,9 @@
 * In thime these should be absorbed into the sample database
 * and can be removed from this file
 *******/
+
+-- Move theatre 2 to site 4
+UPDATE `openeyes`.`ophtroperationbooking_operation_theatre` SET `site_id`='4' WHERE `id`='2';
+
+-- choose a lens for charles darwin
+UPDATE `openeyes`.`et_ophinbiometry_selection` SET `iol_power_right`='25.50', `predicted_refraction_right`='-0.17', `lens_id_right`='5', `formula_id_left`='0', `formula_id_right`='1' WHERE `id`='26';
