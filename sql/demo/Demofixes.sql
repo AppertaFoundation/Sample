f8204f85a64b0e6d6d670993b82848bd71b3bdec
biskyt <toby.bisco@openeyes.org.uk>
2017-09-14 11:45:55 +0100

    change name of POS to POH

--- sql/demo/Demofixes.sql
+++ sql/demo/Demofixes.sql
@@ -3,3 +3,4 @@
 * In thime these should be absorbed into the sample database
 * and can be removed from this file
 *******/
+UPDATE element_type set name = 'Previous Ophthalmic History' where class_name = 'OEModule\\OphCiExamination\\models\\PastSurgery';
