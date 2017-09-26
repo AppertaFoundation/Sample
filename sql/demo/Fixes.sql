bedd8d6114fe0714a79c67beaa4c638daee834f4
biskyt <toby.bisco@openeyes.org.uk>
2017-09-26 13:16:12 +0100

    added fixes

---
+++ sql/demo/Fixes.sql
@@ -0,0 +1,2 @@
+/***** This file contains fixes that should be merged into the sample data before release *****/
+UPDATE `openeyes`.`setting_metadata` SET `name`='Disable copy for notes in correspondence' WHERE `id`='50';
