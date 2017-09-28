a13b6a0d42794616c7b36008645efe1fed75b463
Biskyt <github@gettheitguy.com>
2017-09-28 13:27:27 +0000

    Fix wards for theatres

--- sql/demo/Fixes.sql
+++ sql/demo/Fixes.sql
@@ -6,3 +6,9 @@ delete from ophtroperationbooking_operation_erod_rule;
 
 -- fix issue with emergency diary, which is looking for AE
 UPDATE `openeyes`.`subspecialty` SET `ref_spec`='AE' WHERE `id`='1';
+
+--fix wards for booking
+UPDATE `openeyes`.`ophtroperationbooking_operation_theatre` SET `ward_id`='1' WHERE `id`='1';
+UPDATE `openeyes`.`ophtroperationbooking_operation_theatre` SET `ward_id`='6' WHERE `id`='2';
+UPDATE `openeyes`.`ophtroperationbooking_operation_theatre` SET `ward_id`='4' WHERE `id`='3';
+UPDATE `openeyes`.`ophtroperationbooking_operation_theatre` SET `ward_id`='6' WHERE `id`='4';
