41fbf082d1b6934bfb7db2654cccb52d1cfbcbb0
biskyt <toby.bisco@openeyes.org.uk>
2017-09-28 08:38:13 +0100

    fixes and test macro

--- sql/demo/Fixes.sql
+++ sql/demo/Fixes.sql
@@ -1,2 +1,5 @@
 /***** This file contains fixes that should be merged into the sample data before release *****/
 UPDATE `openeyes`.`setting_metadata` SET `name`='Disable copy for notes in correspondence' WHERE `id`='50';
+
+delete from ophtroperationbooking_operation_erod_rule_item;
+delete from ophtroperationbooking_operation_erod_rule;
