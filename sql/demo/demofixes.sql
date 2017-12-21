3bd26288ab5d1f3154d47dfb68d89cdcd4700b1f
biskyt <github@gettheitguy.com>
2017-12-21 15:46:52 +0000

    Some fixes to base data

--- sql/demo/demofixes.sql
+++ sql/demo/demofixes.sql
@@ -1,1 +1,9 @@
 use openeyes;
+DELETE FROM ophtroperationbooking_waiting_list_contact_rule;
+DELETE FROM `ophcocorrespondence_firm_site_secretary`;
+
+TRUNCATE TABLE ophcotherapya_email_recipient;
+INSERT INTO ophcotherapya_email_recipient (id, site_id, recipient_name, recipient_email, type_id, display_order)
+VALUES (1, 1, 'AMD Group', 'amdgroup@example.openeyes.org', 1, 1);
+INSERT INTO ophcotherapya_email_recipient (id, site_id, recipient_name, recipient_email, type_id, display_order)
+VALUES (2, 1, 'AMD Group', 'amdgroup@example.openeyes.org', 2, 2);
