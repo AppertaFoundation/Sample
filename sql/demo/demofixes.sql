692ddacf56f5f23672baef3e93dc90a90fa08c54
biskyt <github@gettheitguy.com>
2017-12-22 12:31:07 +0000

    more fixes

--- sql/demo/demofixes.sql
+++ sql/demo/demofixes.sql
@@ -7,3 +7,12 @@ INSERT INTO ophcotherapya_email_recipient (id, site_id, recipient_name, recipien
 VALUES (1, 1, 'AMD Group', 'amdgroup@example.openeyes.org', 1, 1);
 INSERT INTO ophcotherapya_email_recipient (id, site_id, recipient_name, recipient_email, type_id, display_order)
 VALUES (2, 1, 'AMD Group', 'amdgroup@example.openeyes.org', 2, 2);
+
+UPDATE firm SET consultant_id = NULL WHERE `name` = 'Cataract Service';
+UPDATE firm SET pas_code = NULL, `name` = 'Medical Retinal Service', consultant_id = NULL, can_own_an_episode = 1, runtime_selectable = 0 WHERE id = 1;
+DELETE FROM firm where id = 9;
+
+TRUNCATE TABLE user_session;
+
+UPDATE ophinbiometry_calculation_formula SET last_modified_user_id = 1, created_user_id = 1;
+UPDATE ophinbiometry_lenstype_lens SET last_modified_user_id = 1, created_user_id = 1;
